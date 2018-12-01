06acb45878b722e75ca5300891ab174ef022be9e
Ryoma HOSHINO <kita2conananadon@gmail.com>
2018-12-01 13:57:13 +0900

    App.vue変更

--- App.vue
+++ App.vue
@@ -1,9 +1,64 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
-    </view>
+    <text>
+      {{ myMessage }}
+    </text>
+    <touchable-opacity class="button" :on-press="sayHello">
+      <text class="button-text">ボタン</text>
+    </touchable-opacity>
+    <!-- <image :source="image" /> -->
+  </view>
 </template>
  
+ <script>
+  // import { RNCamera } from 'react-native-camera';
+  // import { Camera, Permissions } from 'expo';
+  // import { Toast } from 'native-base';
+
+  export default {
+    data() {
+      return {
+        myMessage: 'こんにちは！',
+        hasCameraPermission: false,
+        // type: Camera.Constants.Type.back,
+        image: {
+          uri: ''
+        }
+      }
+    },
+    methods: {
+      sayHello() {
+        alert('Hello, World !');
+        this.myMessage = '文字を変えてみた';
+      },
+      takePhoto() {
+        this.launchCamera();
+      },
+      launchCamera() {
+        // console.log(this.RNCamera);
+        // this.RNCamera.takePictureAsync()
+        //   .then(data => {
+        //     Toast.show({
+        //       text: 'Success',
+        //       buttonText: 'Okay',
+        //       duration: 2000,
+        //     });
+        //     this.image.uri = pictureData.url;
+        //   });
+      }
+    },
+    mounted() {
+      // Permissions.askAsync(Permissions.CAMERA)
+      //   .then(status => {
+      //     hasCameraPermission = status.status == "granted" ? true : false;
+      //   }).catch((err) => {
+      //       console.log(err);
+      //   });
+    }
+    // components: { RNCamera, Camera },
+  }
+ 
+ </script>
 <style>
 .container {
   background-color: white;
@@ -13,5 +68,16 @@
 }
 .text-color-primary {
   color: blue;
+  font-size: 10px;
+}
+.button {
+  background-color: #3358d3;
+  padding: 12px 48px;
+  border-radius: 4px;
+  margin-top: 12px;
+}
+.button-text {
+  font-size: 18px;
+  color: #ffffff;
 }
 </style>
